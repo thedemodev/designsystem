4d21d274e29e619c009fe5078b062113586d1758
Kristofer Walters <kristofer.walters@sparebank1.no>
2020-02-20 14:43:50 +0100

    fix: give it to me, prettier

--- src/assets/darkmode-switch.js
+++ src/assets/darkmode-switch.js
@@ -1,18 +1,116 @@
-(function() {
-    var darkModeEnabled = false;
-    var button = document.getElementsByClassName('darkmode-button')[0];
-    var checkbox = document.getElementsByClassName(
-        'darkmode-button__switch',
-    )[0];
+/*eslint no-var:0,vars-on-top:1,no-param-reassign:0,no-use-before-define:["error",{"functions":false}] */ (function() {
+    var userControls = initUserControls();
 
-    function isDarkModeSupported() {
-        return window.matchMedia('(prefers-color-scheme)').media !== 'not all';
+    document.addEventListener('DOMContentLoaded', function() {
+        if (getDarkModeCookieValue()) {
+            userControls.enableDarkMode();
+        }
+    });
+
+    listenToOsDarkModeChanges();
+
+    function listenToOsDarkModeChanges() {
+        var ua = navigator.userAgent.toLowerCase();
+        var isSafari =
+            ua.indexOf('safari') !== -1 && ua.indexOf('chrome') !== -1;
+        var mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+
+        if (isSafari) {
+            mediaQueryList.addListener(onOsDarkModeSelected);
+        } else {
+            mediaQueryList.addEventListener('change', onOsDarkModeSelected);
+        }
+
+        function onOsDarkModeSelected() {
+            if (getDarkModeEnabledByOS()) {
+                userControls.disableUserInput();
+                turnOnDarkMode();
+            } else {
+                userControls.allowUserInput();
+                turnOffDarkMode();
+            }
+        }
     }
 
-    /*
-Loop through all loaded style sheets, and alter the media query condition text that matches `fromText` with `toText`
-Used to change dark mode media query to be applied by changing it from `(prefers-color-scheme: dark)` to `only screen`
- */
+    function initUserControls() {
+        var label = document.querySelector('.darkmode-button__label');
+        var button = document.querySelector('.darkmode-button');
+        var checkbox = document.querySelector('.darkmode-button__switch');
+
+        button.addEventListener('click', function(event) {
+            if (!isDarkModeSupported()) {
+                alert('Sorry! Darkmode is not supported with this browser.');
+                event.preventDefault();
+            }
+
+            function isDarkModeSupported() {
+                return (
+                    window.matchMedia('(prefers-color-scheme)').media !==
+                    'not all'
+                );
+            }
+        });
+
+        button.addEventListener('keydown', function(event) {
+            if (event.keyCode === 32 || event.keyCode === 13) {
+                checkbox.click();
+            }
+        });
+
+        checkbox.addEventListener('change', function() {
+            var checked = this.checked;
+
+            animateCssTransition();
+            if (checked) {
+                turnOnDarkMode();
+            } else {
+                turnOffDarkMode();
+            }
+            setDarkModeCookie(checked);
+        });
+
+        return {
+            enableDarkMode: function() {
+                checkbox.click();
+            },
+            disableUserInput: function() {
+                checkbox.checked = true;
+                checkbox.disabled = true;
+                label.classList.add('darkmode-button__switch--inputDisabled');
+            },
+            allowUserInput: function() {
+                label.classList.remove(
+                    'darkmode-button__switch--inputDisabled',
+                );
+                checkbox.checked = false;
+                checkbox.disabled = false;
+            },
+        };
+    }
+
+    function turnOnDarkMode() {
+        swapMediaQueryConditionText(
+            '(prefers-color-scheme: dark)',
+            'only screen',
+        );
+
+        document.body.classList.add('native');
+    }
+
+    function turnOffDarkMode() {
+        swapMediaQueryConditionText(
+            'only screen',
+            '(prefers-color-scheme: dark)',
+        );
+
+        document.body.classList.remove('native');
+    }
+
+    /** Loop through all loaded style sheets, and alter the media query
+     * condition text that matches `fromText` with `toText` Used to change dark
+     * mode media query to be applied by changing it from
+     * `(prefers-color-scheme: dark)` to `only screen`.
+     */
     function swapMediaQueryConditionText(fromText, toText) {
         var sheets = document.styleSheets;
         for (var sheetIndex = 0; sheetIndex < sheets.length; ++sheetIndex) {
@@ -29,124 +127,36 @@ Used to change dark mode media query to be applied by changing it from `(prefers
                         rule.media.mediaText = toText;
                     }
                 }
-            } catch (o_O) {
-                console.error(o_O);
-                console.error(sheet);
+            } catch (err) {
+                // nevermind
             }
         }
     }
 
-    function onDarkModeSelected() {
-        swapMediaQueryConditionText(
-            '(prefers-color-scheme: dark)',
-            'only screen',
-        );
-
-        document.body.classList.add('native');
-        document.querySelectorAll('.darkmode-button__icon').forEach(icon => {
-            icon.style.filter = 'invert(100%)';
-        });
+    function getDarkModeEnabledByOS() {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches;
     }
 
-    function onLightModeSelected() {
-        swapMediaQueryConditionText(
-            'only screen',
-            '(prefers-color-scheme: dark)',
-        );
-
-        document.body.classList.remove('native');
-        document.querySelectorAll('.darkmode-button__icon').forEach(icon => {
-            icon.style.filter = '';
-        });
-    }
-
-    function setCookie(darkmodeEnabled) {
+    function setDarkModeCookie(darkmodeEnabled) {
         var date = new Date();
         date.setTime(date.getTime() + 24 * 60 * 60 * 1000);
-        document.cookie = `sb1design-darkmode=${darkmodeEnabled}; expires=${date.toGMTString()}; path=/`;
+        document.cookie = `sb1design-darkmode=${
+            darkmodeEnabled ? 'true' : 'false'
+        }; expires=${date.toGMTString()}; path=/`;
     }
 
-    let animateCssTransition = () => {
+    function getDarkModeCookieValue() {
+        return (
+            document.cookie.split(';').filter(function(c) {
+                return c.startsWith('sb1design-darkmode=true');
+            }).length > 0
+        );
+    }
+
+    function animateCssTransition() {
         document.documentElement.classList.add('transition');
         window.setTimeout(() => {
             document.documentElement.classList.remove('transition');
         }, 1000);
-    };
-
-    function isSafari() {
-        var ua = navigator.userAgent.toLowerCase();
-        if (ua.indexOf('safari') != -1 && ua.indexOf('chrome') != -1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    button.addEventListener('click', function(event) {
-        if (!isDarkModeSupported()) {
-            alert('Sorry! Darkmode is not supported with this browser.');
-            event.preventDefault();
-        }
-    });
-
-    checkbox.addEventListener('change', function() {
-        if (this.checked) {
-            darkModeEnabled = true;
-            animateCssTransition();
-            onDarkModeSelected();
-            setCookie(true);
-        } else {
-            darkModeEnabled = false;
-            animateCssTransition();
-            onLightModeSelected();
-            setCookie(false);
-        }
-    });
-
-    document.addEventListener('DOMContentLoaded', function(event) {
-        if (
-            document.cookie.split(';').filter(function(c) {
-                return c.startsWith('sb1design-darkmode=true');
-            }).length > 0
-        ) {
-            checkbox.click();
-        }
-    });
-
-    var dmButton = document.querySelector('button.darkmode-button');
-
-    dmButton.addEventListener('keydown', function(e) {
-        if (e.keyCode === 32 || e.keyCode === 13) {
-            checkbox.click();
-        }
-    });
-
-    var mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
-
-    function onOsDarkModeSelected() {
-        var darkModeEndabledByOS = window.matchMedia(
-            '(prefers-color-scheme: dark)',
-        ).matches;
-        var labelEl = document.getElementsByClassName(
-            'darkmode-button__label',
-        )[0];
-
-        // disable manual swith if dark mode is enabled OS-wide
-        checkbox.checked = darkModeEndabledByOS;
-        darkModeEndabledByOS
-            ? labelEl.classList.add('darkmode-button__switch--inputDisabled')
-            : labelEl.classList.remove(
-                  'darkmode-button__switch--inputDisabled',
-              );
-        darkModeEndabledByOS
-            ? (checkbox.disabled = true)
-            : (checkbox.disabled = false);
-        darkModeEndabledByOS ? onDarkModeSelected() : onLightModeSelected();
-    }
-
-    if (isSafari) {
-        mediaQueryList.addListener(onOsDarkModeSelected);
-    } else {
-        mediaQueryList.addEventListener('change', onOsDarkModeSelected);
     }
 })();
